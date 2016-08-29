a8a4989643856b9b40977f9f82e563b6924b054c
Kurt Gilpin <kurt@Gilpin.info>
2016-08-29 17:55:55 -0500

    Add in alternate patcher script and executables

---
+++ Updates/Update.bat
@@ -0,0 +1,28 @@
+echo OFF
+NET SESSION >nul 2>&1
+IF %ERRORLEVEL% EQU 0 (
+    ECHO Administrator PRIVILEGES Detected! 
+    ECHO This will update your current directory with the
+    ECHO latest Vegarlson Asylum client files.
+    PAUSE
+    rsync.exe -avz update.vegarlson-server.org::vaclient .
+
+) ELSE (
+   echo ######## ########  ########   #######  ########  
+   echo ##       ##     ## ##     ## ##     ## ##     ## 
+   echo ##       ##     ## ##     ## ##     ## ##     ## 
+   echo ######   ########  ########  ##     ## ########  
+   echo ##       ##   ##   ##   ##   ##     ## ##   ##   
+   echo ##       ##    ##  ##    ##  ##     ## ##    ##  
+   echo ######## ##     ## ##     ##  #######  ##     ## 
+   echo.
+   echo.
+   echo ####### ERROR: ADMINISTRATOR PRIVILEGES REQUIRED #########
+   echo This script must be run as administrator to work properly!  
+   echo If you're seeing this after clicking on a start menu icon, then right click on the shortcut and select "Run As Administrator".
+   echo ##########################################################
+   echo.
+   PAUSE
+   EXIT /B 1
+)
+@echo ON
