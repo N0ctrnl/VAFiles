584877ba1f2246e7c6e0ae0cffe33fd290945529
U-KTF\kurt <kurt@Monster.ktf.local>
2018-02-06 19:52:04 -0600

    Adding files for new patch method

---
+++ rof2/README.txt
@@ -0,0 +1,3 @@
+Extract the contents of patch.zip to your root EQ directory.
+Also delete the following files:
+dummy.txt
---
+++ rof2/Update.bat
@@ -0,0 +1,65 @@
+@echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+echo OFF
+NET SESSION >nul 2>&1
+IF %ERRORLEVEL% EQU 0 (
+    ECHO Administrator PRIVILEGES Detected! 
+    ECHO This will update your current directory with the
+    ECHO latest Vegarlson Asylum client files.
+    PAUSE
+    echo Making sure your EQ directory has write permissions.
+    attrib -R /S
+    @echo off
+    for %%n IN (08,09) DO del .\uifiles\default\loadscreens\loadscreen%%n.jpg 2>nul
+    for /L %%n IN (10,1,55) DO del .\uifiles\default\loadscreens\loadscreen%%n.jpg 2>nul
+    del update.exe 2>nul
+    rsync.exe -avz update.vegarlson-server.org::vaclient .
+    PAUSE
+
+) ELSE (
+   echo ######## ########  ########   #######  ########  
+   echo ##       ##     ## ##     ## ##     ## ##     ## 
+   echo ##       ##     ## ##     ## ##     ## ##     ## 
+   echo ######   ########  ########  ##     ## ########  
+   echo ##       ##   ##   ##   ##   ##     ## ##   ##   
+   echo ##       ##    ##  ##    ##  ##     ## ##    ##  
+   echo ######## ##     ## ##     ##  #######  ##     ## 
+   echo.
+   echo.
+   echo ####### ERROR: ADMINISTRATOR PRIVILEGES REQUIRED #########
+   echo This script must be run as administrator to work properly!  
+   echo If you're seeing this after clicking on a start menu icon, then right click on the shortcut and select "Run As Administrator".
+   echo ##########################################################
+   echo.
+   PAUSE
+   EXIT /B 1
+)
+@echo ON
